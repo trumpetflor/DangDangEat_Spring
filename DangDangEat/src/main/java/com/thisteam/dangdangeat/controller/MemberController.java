3d0c9d6f8e92a941f1e41fe11ed645f0f48b9e92
chz5kjk0 <chz5kjk0@gmail.com>
2023-01-21 17:29:39 +0900

    < 230121 - jk > - MemberVO 생성 중 - MemberController 생성 중

--- DangDangEat/src/main/java/com/thisteam/dangdangeat/controller/MemberController.java
+++ DangDangEat/src/main/java/com/thisteam/dangdangeat/controller/MemberController.java
@@ -1,9 +1,17 @@
 package com.thisteam.dangdangeat.controller;
 
+import javax.servlet.http.HttpSession;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 
 import com.thisteam.dangdangeat.service.MemberService;
+import com.thisteam.dangdangeat.vo.MemberVO;
 
 @Controller
 public class MemberController {
@@ -11,6 +19,33 @@ public class MemberController {
 	@Autowired
 	private MemberService service;
 	
+	
+	
+	// 회원가입 결과
+	@GetMapping(value = "/MemberJoinResult.me")
+	public String joinResult() {
+		return "member/member_join_result"; // member_join_result.jsp 로 포워딩
+	}
+	
+	// 로그인 페이지
+	@GetMapping(value = "/MemberLoginForm.me")
+	public String login() {
+		return "member/login"; // login.jsp 로 포워딩
+	}
+
+	// 로그인 프로
+	@PostMapping(value = "/MemberLoginPro.me")
+	public String loginPro(@ModelAttribute MemberVO member, Model model, HttpSession session) {
+		
+		// 비밀번호 암호화 !!DB에 비밀번호 재설정 필요(해싱, 솔팅 다름)!!
+		BCryptPasswordEncoder passwdEncoder = new BCryptPasswordEncoder(); // 객체 생성
+//		String passwd = service.getPasswd(member.getId());
+		
+		
+		
+		return "";
+	}
+
 	
 }
 
--- DangDangEat/src/main/java/com/thisteam/dangdangeat/vo/MemberVO.java
+++ DangDangEat/src/main/java/com/thisteam/dangdangeat/vo/MemberVO.java
@@ -1,7 +1,24 @@
 package com.thisteam.dangdangeat.vo;
 
 /*
-  
+-----------------+------------+----+---+-------+-----+
+Field            |Type        |Null|Key|Default|Extra|
+-----------------+------------+----+---+-------+-----+
+member_id        |varchar(20) |NO  |PRI|       |     |
+member_pass      |varchar(200)|NO  |   |       |     |
+member_email     |varchar(50) |NO  |UNI|       |     |
+member_name      |varchar(20) |NO  |   |       |     |
+member_mobile    |varchar(15) |NO  |   |       |     |
+member_addr1     |varchar(100)|NO  |   |       |     |
+member_addr2     |varchar(100)|NO  |   |       |     |
+member_postcode  |varchar(6)  |NO  |   |       |     |
+member_date      |date        |NO  |   |       |     |
+member_point     |int         |YES |   |       |     |
+member_birth     |date        |YES |   |       |     |
+member_admin     |varchar(1)  |YES |   |N      |     |
+member_status    |varchar(1)  |YES |   |       |     |
+member_authStatus|varchar(1)  |YES |   |N      |     |
+-----------------+------------+----+---+-------+-----+
  */
 public class MemberVO {
 	
