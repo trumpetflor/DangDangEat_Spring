5286773139654e4145bcab4905ce7084ecb17753
yeram <118324239+sourgummy@users.noreply.github.com>
2023-01-30 14:12:38 +0900

    23/01/30 jakyoung AdminController
    회원 삭제 메서드 수정
    
    AdminMemberDelete
    memberDelete()

--- DangDangEat/src/main/java/com/thisteam/dangdangeat/controller/AdminController.java
+++ DangDangEat/src/main/java/com/thisteam/dangdangeat/controller/AdminController.java
@@ -37,6 +37,7 @@ public class AdminController {
 	
 
 // ============================================== jakyoung ================================================
+	
 	@GetMapping(value = "AdminMemberList")
 	public String memberList(
 			Model model,
@@ -91,42 +92,51 @@ public class AdminController {
 		
 	}
 	
-	// 회원 삭제
-	@GetMapping(value = "AdminMemberDelete")
-	public String memberDelete(
-			@RequestParam String id,
-			Model model,
-			HttpSession session
-				) {
-			
-			String sid = (String)session.getAttribute("sId");
-			
-			if(id == null || sid.equals("") || !sid.equals("admin")) { // 세션 아이디가 null 이거나 "" 이거나 admin 이 아닐 경우
-				model.addAttribute("msg", "잘못된 접근입니다!");
-				return "redirect:/";
-			} else { // 세션 아이디 있을 경우
-				// MemberVO 객체 생성 및 파라미터 저장
-				MemberVO member = new MemberVO();
-				member.setMember_id(id);
-				
-				// 회원 삭제
-				int deleteCount = service.memberDelete(member);
-				
-				if(deleteCount > 0) { // 회원 삭제 성공
-					return "redirect:AdminMemberList";
-				} else {
-					model.addAttribute("msg", "회원 삭제 실패");
-					return "fail_back";
-				}
-				
-			}
-  }			
+   // 회원 삭제
+   @GetMapping(value = "AdminMemberDelete")
+   public String memberDelete(
+         @RequestParam String id,
+         Model model,
+         HttpSession session
+            ) {
+         
+         String sid = (String)session.getAttribute("sId");
+         
+         if(id == null || sid.equals("") || !sid.equals("admin")) { // 세션 아이디가 null 이거나 "" 이거나 admin 이 아닐 경우
+            model.addAttribute("msg", "잘못된 접근입니다!");
+            return "redirect:/";
+         } else { // 세션 아이디 있을 경우
+            // MemberVO 객체 생성 및 파라미터 저장
+            MemberVO member = new MemberVO();
+            member.setMember_id(id);
+            
+            // 회원 삭제 수행
+            int deleteCount = 0;
+            try {
+               deleteCount = service.memberDelete(member);
+            } catch (DataIntegrityViolationException e) {
+               System.out.println("java.sql.SQLIntegrityConstraintViolationException"
+                     + ": Cannot delete or update a parent row"
+                     + ": a foreign key constraint fails"
+                     + " (`class5_220823_team2`.`auth`, CONSTRAINT `auth_ibfk_1` FOREIGN KEY (`auth_id`) REFERENCES `member` (`member_id`))");
+               e.printStackTrace();
+            }
+            
+            if(deleteCount > 0) { // 회원 삭제 성공
+               return "redirect:AdminMemberList";
+            } else {
+               model.addAttribute("msg", "회원 삭제 실패!");
+               return "fail_back";
+            }
+            
+         }
+		
 
 
 	
   // ============================================== hawon ================================================
   
-  //관리자 쿠폰등록 view페이지
+ 	//관리자 쿠폰등록 view페이지
 	@GetMapping(value = "AdminCouponRegister")
 	public String CouponRegister() {
 		return "admin/admin_coupon";
@@ -138,12 +148,12 @@ public class AdminController {
 		
 		
 		
-		//admin_couponList.jsp에서 보여질 쿠폰 리스트 JSONArray형태로 request객체에 저장
+		// admin_couponList.jsp에서 보여질 쿠폰 리스트 JSONArray형태로 request객체에 저장
 //		JSONArray CouponList =  odService.SelectCouponList();
 //		System.out.println(CouponList +" - "+this.getClass());
 //		model.setAttribute("CouponList", CouponList);
 		
-		//쿠폰 개수 리턴받음
+		// 쿠폰 개수 리턴받음
 		int couponCount = odService.getCouponTotalAmount();
 //		model.addAttribute("couponCount", CouponList);
 		
