71c015e04fdb3f4bea8732d8b8365fcd5bb47966
yeram <creamy.9416@gmail.com>
2023-01-31 19:29:53 +0900

    < 23/01/31 > Cart Insert
    장바구니 추가 작업 중

--- DangDangEat/src/main/java/com/thisteam/dangdangeat/service/CartService.java
+++ DangDangEat/src/main/java/com/thisteam/dangdangeat/service/CartService.java
@@ -14,10 +14,23 @@ public class CartService {
 	@Autowired
 	private CartMapper mapper;
 	
+	
+	// 장바구니 담기
+	public int insertCart(CartVO cart) {
+		return mapper.insertCart(cart);
+	}
+	
+	
+	// 장바구니 중복상품 확인
+	public String isCartExist(CartVO cart) {
+		return mapper.isCartExist(cart);
+	}
+	
 	// 장바구니 목록
 	public List<CartVO> getCartList(String member_id) {
 		return mapper.selectCartList(member_id);
 	}
+	
 	
 	
 
